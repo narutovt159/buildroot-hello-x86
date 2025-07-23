270184941fe0b3839933acf11e66b46921d48918
narutovt159 <59289532+narutovt159@users.noreply.github.com>
2025-07-23 05:47:59 -0300

    Update hello.mk

--- hello/hello.mk
+++ hello/hello.mk
@@ -1,20 +1,15 @@
 HELLO_VERSION = 1.0
-HELLO_SITE = .
+HELLO_SITE = $(TOPDIR)/package/hello
 HELLO_SITE_METHOD = local
 HELLO_LICENSE = Public Domain
 
 define HELLO_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	$(TARGET_CC) $(TARGET_CFLAGS) -o $(@D)/hello $(@D)/hello.c
 endef
 
 define HELLO_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/hello $(TARGET_DIR)/usr/bin/hello
+	$(INSTALL) -D -m 0755 $(TOPDIR)/package/hello/init/hello.init $(TARGET_DIR)/etc/init.d/S99hello
 endef
-
-define HELLO_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/hello $(TARGET_DIR)/usr/bin/hello
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL)/package/hello/init/hello.init $(TARGET_DIR)/etc/init.d/S99hello
-endef
-
 
 $(eval $(generic-package))
